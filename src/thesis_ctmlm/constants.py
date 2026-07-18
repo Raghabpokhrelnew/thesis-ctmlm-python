48407ca294fa776aed4b9da953849ba6d2ae0d1b
Raghabpokhrelnew <105359195+Raghabpokhrelnew@users.noreply.github.com>
2026-07-18 19:33:38 +0200

    Add physical constants

---
+++ src/thesis_ctmlm/constants.py
@@ -0,0 +1,9 @@
+"""Physical constants used by the thesis CTMLM package."""
+
+CP = 1004.0          # J kg-1 K-1, specific heat of dry air
+RD = 287.0           # J kg-1 K-1, gas constant for dry air
+LV = 2.5e6           # J kg-1, latent heat of vaporization
+G = 9.81             # m s-2, gravitational acceleration
+RHO0 = 1.0           # kg m-3, reference air density
+P0 = 100000.0        # Pa, reference pressure
+SECONDS_PER_DAY = 86400.0
